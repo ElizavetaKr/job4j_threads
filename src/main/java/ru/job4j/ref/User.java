4d48386ed2e8d2aca6f7d0b1cf852fdd8114f958
ElizavetaKr <elizovetakryukova@mail.ru>
2024-11-25 20:11:22 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/ref/User.java
@@ -0,0 +1,20 @@
+package ru.job4j.ref;
+
+public class User {
+    private int id;
+    private String name;
+
+    public static User of(String name) {
+        User user = new User();
+        user.name = name;
+        return user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
